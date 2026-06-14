fa3d19bc89c1bd3b0154ef54633e808c94feae28
Waqar Ahmed <24pwbcs1370@uetpeshawar.edu.pk>
2026-06-14 23:21:37 +0500

    Fix: Added base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(),tailwindcss()],
+// })
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  base: '/ClientProject-Mern-Stack/', // <-- Bas ye line yahan add kar dein
 })
