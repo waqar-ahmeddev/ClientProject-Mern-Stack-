f757427f440747a8546d52511cdd63911ca5448a
Waqar Ahmed <24pwbcs1370@uetpeshawar.edu.pk>
2026-06-15 01:35:14 +0500

    fixed

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/ClientProject-Mern-Stack-/', // <-- Bas ye line yahan add kar dein
+  base: '/ClientProject-Mern-Stack-', // <-- Bas ye line yahan add kar dein
 })
