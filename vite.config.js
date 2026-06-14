c94f6b0c9fd9eb08e4ace803cbf39351c1063f2a
Waqar Ahmed <24pwbcs1370@uetpeshawar.edu.pk>
2026-06-15 01:52:48 +0500

    Finally Fixing Error after getting  3 hrs on this error

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,3 @@
-// import { defineConfig } from 'vite'
-// import tailwindcss from '@tailwindcss/vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(),tailwindcss()],
-// })
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
@@ -13,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/ClientProject-Mern-Stack-', // <-- Bas ye line yahan add kar dein
+  base: '/ClientProject-Mern-Stack-/', // <-- Bas ye line yahan add kar dein
 })
